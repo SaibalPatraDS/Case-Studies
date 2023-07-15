b3d887ab13c0cc99d0916cfe191a7f2d0697e26f
Saibal Patra <102281722+SaibalPatraDS@users.noreply.github.com>
2023-07-15 19:09:42 +0530

    sql files uploaded on 15-07-2023

---
+++ Case Study of EV_Sales/Data/email_subject.sql
@@ -0,0 +1,19 @@
+CREATE TABLE ev_sales.email_subject(
+   email_subject_id INTEGER  NOT NULL PRIMARY KEY 
+  ,email_subject    VARCHAR(42) NOT NULL
+);
+INSERT INTO ev_sales.email_subject(email_subject_id,email_subject) VALUES (1,'25% off all EVs. It''s a Christmas Time');
+INSERT INTO ev_sales.email_subject(email_subject_id,email_subject) VALUES (2,'A Brand New Scooter...and Car');
+INSERT INTO ev_sales.email_subject(email_subject_id,email_subject) VALUES (3,'A New Year, And Some New EVs');
+INSERT INTO ev_sales.email_subject(email_subject_id,email_subject) VALUES (4,'An Electric Car for a New Age');
+INSERT INTO ev_sales.email_subject(email_subject_id,email_subject) VALUES (5,'Green Cars , Green Friday Offer');
+INSERT INTO ev_sales.email_subject(email_subject_id,email_subject) VALUES (6,'Introducing A Limited Edition');
+INSERT INTO ev_sales.email_subject(email_subject_id,email_subject) VALUES (7,'Sprint Up your Ride');
+INSERT INTO ev_sales.email_subject(email_subject_id,email_subject) VALUES (8,'Save the Planet with some Holiday Savings');
+INSERT INTO ev_sales.email_subject(email_subject_id,email_subject) VALUES (9,'Take out your Electric Scooter for Holiday');
+INSERT INTO ev_sales.email_subject(email_subject_id,email_subject) VALUES (10,'The 2013 FioNex Scooter is Here');
+INSERT INTO ev_sales.email_subject(email_subject_id,email_subject) VALUES (11,'Season of Savings Offer');
+INSERT INTO ev_sales.email_subject(email_subject_id,email_subject) VALUES (12,'Cut you a deal: 20%% off on Parker');
+INSERT INTO ev_sales.email_subject(email_subject_id,email_subject) VALUES (13,'We Really Outdid Ourselves this Year');
+INSERT INTO ev_sales.email_subject(email_subject_id,email_subject) VALUES (14,'Go out with FioNex ! get 10%% off');
+INSERT INTO ev_sales.email_subject(email_subject_id,email_subject) VALUES (15,'Sale is still on this Friday');
